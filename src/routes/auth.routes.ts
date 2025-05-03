3fb4536fd46eb92e7e1102f53fe74820fabb71a7
GmShuvo634 <shuvo@imssystems.tech>
2025-05-03 21:21:03 +0600

    inital commit

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import express, { RequestHandler } from 'express';
+import { login, logout, register } from '../controllers/auth.controller';
+
+const router = express.Router();
+router.post("/register", register as any);
+router.post('/login', login as any);
+router.post('/logout', logout as any);
+
+export default router;
